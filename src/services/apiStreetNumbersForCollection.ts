49beb067921cdc79dc47fc1886cbf65b73e1ee87
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-08-22 11:50:02 +0200

    Initial fetch setup for street numbers

--- src/services/apiStreetNumbersForCollection.ts
+++ src/services/apiStreetNumbersForCollection.ts
@@ -1,5 +1,25 @@
-function getStreetNumbersForCollection() {
-  return fetch(`${import.meta.env.VITE_DI_API_URL}`);
-}
+export async function getStreetNumbersForCollection(streetId: number) {
+  const url = `${
+    import.meta.env.VITE_DI_API_URL
+  }streetNumberSearch/${streetId}?apiKey=${import.meta.env.VITE_DI_API_KEY}`;
 
-export default getStreetNumbersForCollection;
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        referer: import.meta.env.VITE_DI_REFERER,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch street numbers");
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching street numbers:", error);
+  }
+}
