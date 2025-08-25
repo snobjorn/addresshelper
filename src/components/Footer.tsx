ba1d654770705251a8a2f48e7e2f071bf6439d66
Asbjørn Ness <asbjorn.ness@gmail.com>
2025-08-25 08:21:00 +0200

    Minor text change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-gray-900 mt-16 text-white">
       <div className="container mx-auto py-4 text-center">
-        <p>© 2025 Address Helper. All rights reserved.</p>
+        <p>DI</p>
       </div>
     </footer>
   );
